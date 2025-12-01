88e9f1003d801ae87328ee45bc2385146ac736f3
Clara <clarazammar4@gmail.com>
2025-12-01 20:50:06 +0200

    Fix overall layout

--- app.py
+++ app.py
@@ -40,60 +40,130 @@ def render_message(msg):
 
 
 async def main():
-    st.title("Your Travel Buddy")
-    
-    
-    file_path = "/Users/gaelletohme/travel-assistant-chatbot/pictures/dCXxwH.jpg" 
+    file_path = "pictures/dCXxwH.jpg" 
     with open(file_path, "rb") as f:
         data = f.read()
     encoded = base64.b64encode(data).decode()
 
-    page_bg = f"""
+    dark_blue = "rgb(0, 51, 102)"
+    light_blue = "rgb(4 89 173)"
+    styles = f"""
     <style>
-    h1 {{
-    color: black !important;
-    text-shadow: none !important;
-}}
-    [data-testid="stAppViewContainer"] {{
-        background-image: url("data:image/png;base64,{encoded}");
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-    }}
+        h1 {{
+            color: black !important;
+            text-shadow: none !important;
+        }}
 
-    [data-testid="stHeader"] {{
-        background: rgba(0,0,0,0);
-    }}
+        [data-testid="stAppViewContainer"] {{
+            background-image: url("data:image/png;base64,{encoded}");
+            background-size: cover;
+            background-position: bottom;
+            background-repeat: no-repeat;
+        }}
 
-    [data-testid="stSidebar"] {{
-        background: rgba(255,255,255,0.8);
-    }}
-    
-   /*  User message (slightly stronger blue bubble) */
-[data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarUser"]) {{
-    background: rgba(135, 206, 250, 1); 
-    border-radius: 18px;
-    padding: 10px 16px;
-    margin-bottom: 10px;
-}}
+        [data-testid="stHeader"] {{
+            background: {dark_blue};
+            padding: 10px;
+            box-shadow: 0px 4px 12px rgba(0,0,0,0.2) !important;
+        }}
 
-/*  Assistant message (stronger navy bubble) */
-[data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarAssistant"]) {{
-    background: rgba(0, 51, 102, 1); 
-    border-radius: 18px;
-    padding: 10px 16px;
-    margin-bottom: 10px;
-   
-}}
-[data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarAssistant"]) p,
-[data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarAssistant"]) span,
-[data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarAssistant"]) div {{
-    color: white !important;      
-}}
+        [data-testid="stSidebar"] {{
+            background: rgba(255,255,255,0.8);
+        }}
+
+        [data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarUser"]) {{
+            background: {light_blue};
+            border-radius: 18px;
+            padding: 10px 16px;
+            margin-bottom: 10px;
+            margin-top: 20px;
+            width: 65%;
+            align-self: flex-end;
+        }}
+
+        [data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarAssistant"]) {{
+            background: {dark_blue};
+            border-radius: 18px;
+            padding: 10px 16px;
+            margin-bottom: -10px;
+        }}
+
+        [data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarAssistant"]) p,
+        [data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarAssistant"]) span,
+        [data-testid="stChatMessage"]:has([data-testid="stChatMessageAvatarAssistant"]) div {{
+            color: white !important;
+        }}
+
+        .st-emotion-cache-hzygls {{
+            background: transparent;
+            padding: 15px;
+        }}
+
+        [data-testid="stHeader"]::before {{
+            content: "Your Travel Buddy";
+            position: absolute;
+            top: 50%;
+            left: 20px;
+            transform: translateY(-50%);
+            font-size: 28px;
+            font-weight: 900;
+            color: white;
+        }}
+
+        [data-testid="stChatMessageAvatarUser"],
+        [data-testid="stChatMessageAvatarAssistant"] {{
+            display: none;
+        }}
+
+        .st-emotion-cache-6shykm {{
+            padding: 0;
+        }}
+
+        .st-emotion-cache-x1bvup {{
+            max-height: none;
+            height: 50px;
+            border-radius: 30px;
+        }}
+
+        .st-emotion-cache-6shykm,
+         .st-emotion-cache-1cei9z1 {{
+            max-width: 900px;
+            width: 65%;
+        }}
+
+        textarea[placeholder="Your message"] {{
+            min-height: unset;
+            color: 
+        }}
+
+        .st-emotion-cache-sey4o0 {{
+            align-items: center;
+        }}
+
+        .st-emotion-cache-x1bvup:focus-within {{
+            border-color: transparent !important;
+            box-shadow: 0px 4px 12px rgba(0,0,0,0.2) !important;
+        }}
+
+        .st-emotion-cache-x1bvup *,
+        .st-emotion-cache-x1bvup {{
+            background-color: {light_blue};
+        }}
+
+        .st-emotion-cache-vsnu81:disabled, 
+        .st-emotion-cache-vsnu81:disabled:hover, 
+        .st-emotion-cache-vsnu81:disabled:active {{
+            color: rgb(239 241 245);
+        }}
+
+        .st-emotion-cache-1cei9z1 {{
+            padding-bottom: 5rem;
+        }}
     </style>
     """
 
-    st.markdown(page_bg, unsafe_allow_html=True)
+
+    st.markdown(styles, unsafe_allow_html=True)
 
     if "llm" not in st.session_state:
         st.session_state.llm = GeminiChat()
@@ -101,7 +171,7 @@ async def main():
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    # Render hisstory
+    # Render history
     for msg in st.session_state.messages:
         render_message(msg)
 
